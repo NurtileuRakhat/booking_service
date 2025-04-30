459330242d9eac211a117aaf8a1b5b1ba0110000
Nurtileu <raaaaahaaat@gmail.com>
2025-05-01 01:14:14 +0500

    fix telegram

--- internal/usecase/user/interface.go
+++ internal/usecase/user/interface.go
@@ -1,10 +1,1 @@
 package user
-
-import (
-	"booking/internal/entity"
-	"context"
-)
-
-type Service interface {
-	GetUserByID(ctx context.Context, id int64) (*entity.User, error)
-}
--- internal/usecase/user/service.go
+++ internal/usecase/user/service.go
@@ -1,19 +1,1 @@
 package user
-
-import (
-	"booking/internal/entity"
-	"booking/internal/ports/repository"
-	"context"
-)
-
-type UserService struct {
-	repo repository.UserRepository
-}
-
-func NewUserService(repo repository.UserRepository) *UserService {
-	return &UserService{repo: repo}
-}
-
-func (s *UserService) GetUserByID(ctx context.Context, id int64) (*entity.User, error) {
-	return s.repo.GetUserByID(ctx, id)
-}
